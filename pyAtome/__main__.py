c6e2a126a118508e2aff48c021453abb244a2a8e
Pierre <baqs@users.github.com>
2019-08-23 00:11:50 +0200

    Commented-out logging

--- pyAtome/__main__.py
+++ pyAtome/__main__.py
@@ -1,6 +1,7 @@
 import argparse
 import sys
 import json
+# import logging
 
 from pyatome import AtomeClient
 
@@ -12,6 +13,7 @@ def main():
                         required=True, help='Atome username')
     parser.add_argument('-p', '--password',
                         required=True, help='Password')
+    parser.add_argument('--debug', action='store_true', help='Print debug messages to stderr')
     parser.add_argument('action', type=str,
                         default='live', help='Action [live/data]')
     args = parser.parse_args()
@@ -19,11 +21,21 @@ def main():
     client = AtomeClient(args.username, args.password)
 
     try:
+        # if args.debug:
+        #     # You must initialize logging, otherwise you'll not see debug output.
+        #     logging.basicConfig()
+        #     logging.getLogger().setLevel(logging.DEBUG)
+        #     requests_log = logging.getLogger("requests.packages.urllib3")
+        #     requests_log.setLevel(logging.DEBUG)
+        #     requests_log.propagate = True
+
+
+
         client.login()
         if args.action == 'live':
             print(json.dumps(client.get_live(), indent=2))
         else:
-            print "Action not implemented. Please use 'live' as an action."
+            print("Action not implemented. Please use 'live' as an action.")
 
     except BaseException as exp:
         print(exp)
